d911d7995b88105dec301c1843423b32da6eb700
JonissonGomes <joni_gomes@hotmail.com>
2022-04-12 18:00:38 -0300

    Criando visualização de restaurantes

--- frontend/jason-app/src/pages/home/index.tsx
+++ frontend/jason-app/src/pages/home/index.tsx
@@ -10,7 +10,7 @@ const Home = () => {
       address:
         "Rua Conselheiro José Bonifacio, 70 - Cavaleiro, Jaboatão dos Guararapes - PE",
       description:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed consectetur aliquet odio nec gravida. Fusce vitae ornare massa.",
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed consectetur aliquet odio nec gravida. Fusce vitae ornare massa. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed consectetur aliquet odio nec gravida. Fusce vitae ornare massa.",
       urlImage:
         "https://d1csarkz8obe9u.cloudfront.net/posterpreviews/french-restaurant-logo-design-template-2f7896b33ab323cbc1baf240f643e82b_screen.jpg?ts=1597302426",
       manager: "Maximillium Maximus",
@@ -20,7 +20,7 @@ const Home = () => {
       name: "Doce Sabor restaurante",
       address: "Rua Cleto Campelo 44 Sto Antonio, Recife, Pernambuco",
       description:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed consectetur aliquet odio nec gravida. Fusce vitae ornare massa.",
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed consectetur aliquet odio nec gravida. Fusce vitae ornare massa. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed consectetur aliquet odio nec gravida. Fusce vitae ornare massa.",
       urlImage:
         "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQoIG8IEdSohrPhyyd-1LEWfnUo4KSXYt0R7_llr4iOg-LXvIx_Jlt9NVxJgpBrZvN9mwI&usqp=CAU",
       manager: "SweetMarry Brigadeiro",
@@ -30,9 +30,9 @@ const Home = () => {
       name: "Da Base restaurante",
       address: "Parque das nações, Dourados, Mato Grosso do Sul",
       description:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed consectetur aliquet odio nec gravida. Fusce vitae ornare massa.",
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed consectetur aliquet odio nec gravida. Fusce vitae ornare massa. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed consectetur aliquet odio nec gravida. Fusce vitae ornare massa.",
       urlImage:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQoIG8IEdSohrPhyyd-1LEWfnUo4KSXYt0R7_llr4iOg-LXvIx_Jlt9NVxJgpBrZvN9mwI&usqp=CAU",
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRPW8BXSJdIaGUddkJ4mXw9orW2KhHawg690CrTQU8OdqSCRBKx6_5FvAXkvyhqRnC6jsQ&usqp=CAUhttps://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRPW8BXSJdIaGUddkJ4mXw9orW2KhHawg690CrTQU8OdqSCRBKx6_5FvAXkvyhqRnC6jsQ&usqp=CAU",
       manager: "The Database Postgres da Silva",
     },
   ];
@@ -48,8 +48,8 @@ const Home = () => {
             tudo em um só lugar
           </p>
           <Link to={"/home"}>
-            <section className={"buttonPrimary"}>
-              <p className={"textButtonPrimary"}>Começar</p>
+            <section className={"buttonPrimaryHome"}>
+              <p className={"textButtonPrimaryHome"}>Começar</p>
             </section>
           </Link>
         </section>
@@ -58,20 +58,25 @@ const Home = () => {
           <h3 className={"labelTopics"}>Meus restaurantes</h3>
           <hr className={"divisor"} />
 
-          {mockDataRestaurant.map((restaurant) => (
-            <div className={"contentCard"}>
-              <img className={"imageCard"} src={restaurant.urlImage} alt="photo-food" />
-              <h3 className={"titleCard"}>{restaurant.name}</h3>
-              <p className={"descriptionCard"}>{restaurant.description}</p>
-              <button className={"buttonCard"}>
-                <p className={"textButtonCard"}>Ver prato completo</p>
-              </button>
-            </div>
-          ))}
-
-          <h3 className={"labelTopics"}>Cardápio - {}</h3>
-          <hr className={"divisor"} />
+          <div className={'containerCard'}>
+            {mockDataRestaurant.map((restaurant) => (
+              <div className={"contentCard"}>
+                <img
+                  className={"imageCard"}
+                  src={restaurant.urlImage}
+                  alt="photo-food"
+                />
+                <h3 className={"titleCard"}>{restaurant.name}</h3>
+                <p className={"descriptionCard"}>{restaurant.description}</p>
+                <button className={"buttonCard"}>
+                  <p className={"textButtonCard"}>Ver cardápio</p>
+                </button>
+              </div>
+            ))}
+          </div>
         </section>
+          <h3 className={"labelTopics"}>Cardápio - </h3>
+          <hr className={"divisor"} />
       </div>
     </>
   );
