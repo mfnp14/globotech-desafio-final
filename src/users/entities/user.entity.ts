8084cba0008c5f2f765159ce019c6e8c3d50786d
JonissonGomes <joni_gomes@hotmail.com>
2022-04-11 21:20:32 -0300

    Criando entidade [Usuários]

---
+++ src/users/entities/user.entity.ts
@@ -0,0 +1,32 @@
+import {
+  BaseEntity,
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class User extends BaseEntity {
+  @PrimaryGeneratedColumn('increment')
+  id: number;
+
+  @Column({ nullable: false, type: 'varchar', length: 200 })
+  name: string;
+
+  @Column({ nullable: false, type: 'varchar' })
+  email: string;
+
+  @Column({ nullable: false })
+  password: string;
+
+  @Column({ nullable: false })
+  confirmPassword: string;
+
+  @CreateDateColumn()
+  createdDate: Date;
+
+  @UpdateDateColumn()
+  updatedData: Date;
+}
