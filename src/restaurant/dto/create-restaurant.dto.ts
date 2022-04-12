bb202d5a56401815372313793641b223cfe39fab
JonissonGomes <joni_gomes@hotmail.com>
2022-04-12 01:15:37 -0300

    Ajustando identação

--- src/restaurant/dto/create-restaurant.dto.ts
+++ src/restaurant/dto/create-restaurant.dto.ts
@@ -1,8 +1,7 @@
 export class CreateRestaurantDto {
-    name: string;
-    address: string;
-    description: string;
-    logoUrl: string;
-    manager: string;
-  }
-  
+  name: string;
+  address: string;
+  description: string;
+  logoUrl: string;
+  manager: string;
+}
