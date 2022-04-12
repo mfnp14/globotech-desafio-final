c64950886b862d92a819679af45a591f04062438
mfnp14 <marcelfnpontes@hotmail.com>
2022-04-12 00:34:50 -0300

    escrever restaurant service

--- src/restaurant/useCases/createRestaurant/restaurant.service.ts
+++ src/restaurant/useCases/createRestaurant/restaurant.service.ts
@@ -1,4 +1,21 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { CreateRestaurantDto } from 'src/restaurant/dto/create-restaurant.dto';
+import { Restaurant } from 'src/restaurant/entities/restaurant.entity';
+import { RestaurantRepository } from 'src/restaurant/repositories/restaurant.repository';
 
 @Injectable()
-export class RestaurantService {}
+export class RestaurantService {
+    constructor(
+        @InjectRepository(RestaurantRepository)
+        private restaurantRepository: RestaurantRepository
+    ){} 
+    async createNewRestaurant(createRestaurantDto: CreateRestaurantDto): Promise<Restaurant> {
+        if (createRestaurantDto.name === "" || createRestaurantDto.manager === "") {
+            throw new HttpException(
+                'Verifique se suas informações foram preenchidas corretamente',
+                HttpStatus.UNPROCESSABLE_ENTITY,
+            );
+        } return this.restaurantRepository.createRestaurant(createRestaurantDto)
+    }
+}
