a0f3b6385895a8c7f540064bee23097176a7ef57
JonissonGomes <joni_gomes@hotmail.com>
2022-04-12 03:24:38 -0300

    Criando landing Page [ estilização incompleta ]

--- frontend/jason-app/src/pages/landing/index.tsx
+++ frontend/jason-app/src/pages/landing/index.tsx
@@ -1,12 +1,54 @@
-import Header from "../../component/header"
+import { Link } from "react-router-dom";
+import Header from "../../component/header";
+import imagePromotional from "../../assets/pana.png";
+import "./style.css";
 
 const Landing = () => {
   return (
-    <div className={'containerLanding'}>
+    <>
       <Header />
+      <div className={"containerLanding"}>
+        <div className={"contentBrand"}>
+          <section className={"brand"}>
+            <h2 className={"logoLeftBrand"}>{"{"}</h2>
+            <section className={"brandName"}>
+              <h4 className={"firstName"}>JASON</h4>
+              <h4 className={"lastName"}>FOOD</h4>
+            </section>
+            <h2 className={"logoRightBrand"}>{"}"}</h2>
+          </section>
 
-    </div>
-  )
-}
+          <p className={"sloganText"}>
+            Não deixe que a fome te pegue desprevenido, esteja sempre preparado
+            para matar aquilo que está te matando.
+          </p>
 
-export default Landing
+          <p className={"descriptionText"}>
+            Com o Jason você não morre de fome, você pode procurar suas comidas
+            favoritas e ainda criar um menu com base nos dias da semana. Tenha
+            todos os restaurantes na palma da sua mão e adicione seus pratos
+            favoritos de qualquer lugar.
+          </p>
+
+          <section className={"contentButtons"}>
+            <Link to={"/"}>
+              <section className={"buttonSecondary"}>
+                <p className={"textButtonSecondary"}>Criar cardápio</p>
+              </section>
+            </Link>
+
+            <Link to={"/"}>
+              <section className={"buttonPrimary"}>
+                <p className={"textButtonPrimary"}>Criar cardápio</p>
+              </section>
+            </Link>
+          </section>
+        </div>
+
+        <img className={"imagePromotional"} src={imagePromotional} />
+      </div>
+    </>
+  );
+};
+
+export default Landing;
