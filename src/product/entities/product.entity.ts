d1cfefbb15fbc9890cddc388b5ad1ad74ff7167a
JonissonGomes <joni_gomes@hotmail.com>
2022-04-12 20:49:20 -0300

    Criando entidade Product

---
+++ src/product/entities/product.entity.ts
@@ -0,0 +1,35 @@
+import {
+  BaseEntity,
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class Product extends BaseEntity {
+  @PrimaryGeneratedColumn('increment')
+  id: number;
+
+  @Column({ nullable: false, type: 'varchar', length: 200 })
+  name: string;
+
+  @Column({ nullable: false, type: 'varchar' })
+  urlImage: string;
+
+  @Column({ nullable: false })
+  description: string;
+
+  @Column({ nullable: false, type: 'float' })
+  price: number;
+
+  @Column({ nullable: false, array: true })
+  extra: string[];
+
+  @CreateDateColumn()
+  createdDate: Date;
+
+  @UpdateDateColumn()
+  updatedDate: Date;
+}
