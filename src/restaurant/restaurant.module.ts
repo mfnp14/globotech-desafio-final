a79a3404429ac2bb2069f9d12b4c34a5ad3546ef
mfnp14 <marcelfnpontes@hotmail.com>
2022-04-12 01:08:12 -0300

    atualizar restaurant module

--- src/restaurant/restaurant.module.ts
+++ src/restaurant/restaurant.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { RestaurantService } from './useCases/createRestaurant/restaurant.service';
 import { RestaurantController } from './useCases/createRestaurant/restaurant.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Restaurant } from './entities/restaurant.entity';
+import { RestaurantRepository } from './repositories/restaurant.repository';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Restaurant, RestaurantRepository])],
   controllers: [RestaurantController],
   providers: [RestaurantService]
 })
