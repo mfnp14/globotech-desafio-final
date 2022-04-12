365c15385cb71f0fff71c6ceece61324cfea2a67
JonissonGomes <joni_gomes@hotmail.com>
2022-04-11 23:17:15 -0300

    Criando repositório de usuário

--- src/users/respositories/user.repository.ts
+++ src/users/respositories/user.repository.ts
@@ -1,7 +1,8 @@
+import { EntityRepository, Repository } from 'typeorm';
 import { CreateUserDto } from './../dto/create-user.dto';
-import { Repository } from 'typeorm';
 import { User } from '../entities/user.entity';
 
+@EntityRepository(User)
 export class UserRepository extends Repository<User> {
   async createUser(createUserDto: CreateUserDto): Promise<User> {
     const { name, email, password, confirmPassword } = createUserDto;
