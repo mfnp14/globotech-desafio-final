08f3ca737d5f90159dffd03e8c183a354faf7f3b
JonissonGomes <joni_gomes@hotmail.com>
2022-04-11 22:05:47 -0300

    Atualizando importações

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
-import { UsersService } from './users.service';
-import { UsersController } from './users.controller';
+import { UsersService } from './useCases/createUser/users.service';
+import { UsersController } from './useCases/createUser/users.controller';
 
 @Module({
   controllers: [UsersController],
-  providers: [UsersService]
+  providers: [UsersService],
 })
 export class UsersModule {}
