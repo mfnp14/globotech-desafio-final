9974148ec6dc2b718e1177cd097b6d3d08866347
JonissonGomes <joni_gomes@hotmail.com>
2022-04-11 23:06:52 -0300

    Adicionando repository  e Entidade ao módulo

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,8 +1,12 @@
+import { User } from './entities/user.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
 import { UsersService } from './useCases/createUser/users.service';
 import { UsersController } from './useCases/createUser/users.controller';
+import { UserRepository } from './respositories/user.repository';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([User, UserRepository])],
   controllers: [UsersController],
   providers: [UsersService],
 })
