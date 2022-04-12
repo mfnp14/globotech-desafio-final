10182b1078ba86fd94f802e0818400ec6e0d1120
JonissonGomes <joni_gomes@hotmail.com>
2022-04-12 12:45:31 -0300

    Criando footer components

---
+++ frontend/jason-app/src/component/footer/index.tsx
@@ -0,0 +1,10 @@
+import './style.css'
+const Footer = () => {
+  return (
+    <div className={"containerFooter"}>
+    <p className={'textFooter'}>Globotech - Academy</p>
+    </div>
+  )
+}
+
+export default Footer
