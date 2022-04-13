1c63b0d8064c19ff2da32e9d4361b620f37c615a
JonissonGomes <joni_gomes@hotmail.com>
2022-04-12 22:03:00 -0300

    Criando controller de produtos

--- src/product/useCases/createProduct/product.controller.ts
+++ src/product/useCases/createProduct/product.controller.ts
@@ -1,7 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateProductDto } from 'src/product/dto/create-product.dto';
+import { ReturnProductDto } from 'src/product/dto/return-product.dto';
 import { ProductService } from './product.service';
 
 @Controller('product')
 export class ProductController {
   constructor(private readonly productService: ProductService) {}
+  @Post()
+  async createNewProduct(
+    @Body() createProductDto: CreateProductDto,
+  ): Promise<ReturnProductDto> {
+    await this.productService.createNewProduct(createProductDto);
+
+    return {
+      message: 'Produto cadastrado com sucesso',
+    };
+  }
 }
