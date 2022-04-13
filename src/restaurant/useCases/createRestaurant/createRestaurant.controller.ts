f9adaa3a119dd5dd8560646c92a5320bc28e4f4e
JonissonGomes <joni_gomes@hotmail.com>
2022-04-13 00:11:54 -0300

    Renomeando classes e variáveis

--- src/restaurant/useCases/createRestaurant/createRestaurant.controller.ts
+++ src/restaurant/useCases/createRestaurant/createRestaurant.controller.ts
@@ -1,11 +1,11 @@
 import { Body, Controller, Post } from '@nestjs/common';
-import { CreateRestaurantDto } from '../../../restaurant/dto/create-restaurant.dto';
-import { ReturnRestaurantDto } from '../../../restaurant/dto/return-restaurant.dto';
-import { RestaurantService } from './restaurant.service';
+import { CreateRestaurantDto } from '../../dto/create-restaurant.dto';
+import { ReturnRestaurantDto } from '../../dto/return-restaurant.dto';
+import { CreateRestaurantService } from './createRestaurant.service';
 
 @Controller('restaurant')
-export class RestaurantController {
-  constructor(private readonly restaurantService: RestaurantService) {}
+export class CreateRestaurantController {
+  constructor(private readonly restaurantService: CreateRestaurantService) {}
 
   @Post()
   async createNewRestaurant(
