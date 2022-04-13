1f6c6357198b3d5c540686cdd411ab0506487578
mfnp14 <marcelfnpontes@hotmail.com>
2022-04-12 21:28:13 -0300

    escrever product service file

--- src/product/useCases/createProduct/product.service.ts
+++ src/product/useCases/createProduct/product.service.ts
@@ -1,4 +1,21 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { CreateProductDto } from 'src/product/dto/create-product.dto';
+import { Product } from 'src/product/entities/product.entity';
+import { ProductRepository } from 'src/product/repositories/createProduct.repository';
 
 @Injectable()
-export class ProductService {}
+export class ProductService {
+    constructor(
+        @InjectRepository(ProductRepository)
+        private productRepository: ProductRepository
+    ){} 
+    async createNewProduct(createProductDto: CreateProductDto): Promise<Product> {
+        if (createProductDto.name === "" || createProductDto.price === null) {
+            throw new HttpException(
+                'Verifique se suas informações foram preenchidas corretamente',
+                HttpStatus.UNPROCESSABLE_ENTITY,
+            );
+        } return this.productRepository.createProduct(createProductDto)
+    }
+}
