7eb62848a3ebeffdd18f3d2d263bcc746deff249
JonissonGomes <joni_gomes@hotmail.com>
2022-04-12 21:49:24 -0300

    Ajustando identação

--- src/product/useCases/createProduct/product.service.ts
+++ src/product/useCases/createProduct/product.service.ts
@@ -6,16 +6,17 @@ import { ProductRepository } from 'src/product/repositories/createProduct.reposi
 
 @Injectable()
 export class ProductService {
-    constructor(
-        @InjectRepository(ProductRepository)
-        private productRepository: ProductRepository
-    ){} 
-    async createNewProduct(createProductDto: CreateProductDto): Promise<Product> {
-        if (createProductDto.name === "" || createProductDto.price === null) {
-            throw new HttpException(
-                'Verifique se suas informações foram preenchidas corretamente',
-                HttpStatus.UNPROCESSABLE_ENTITY,
-            );
-        } return this.productRepository.createProduct(createProductDto)
+  constructor(
+    @InjectRepository(ProductRepository)
+    private productRepository: ProductRepository,
+  ) {}
+  async createNewProduct(createProductDto: CreateProductDto): Promise<Product> {
+    if (createProductDto.name === '' || createProductDto.price === null) {
+      throw new HttpException(
+        'Verifique se suas informações foram preenchidas corretamente',
+        HttpStatus.UNPROCESSABLE_ENTITY,
+      );
     }
+    return this.productRepository.createProduct(createProductDto);
+  }
 }
