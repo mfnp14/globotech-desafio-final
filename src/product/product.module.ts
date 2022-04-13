6f11171da6f2a4e2400a4d5c5b1b3a1f037014ae
JonissonGomes <joni_gomes@hotmail.com>
2022-04-12 21:50:49 -0300

    Adicionando Entidade e Repositorio

--- src/product/product.module.ts
+++ src/product/product.module.ts
@@ -1,8 +1,12 @@
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
 import { ProductService } from './useCases/createProduct/product.service';
 import { ProductController } from './useCases/createProduct/product.controller';
+import { Product } from './entities/product.entity';
+import { ProductRepository } from './repositories/createProduct.repository';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Product, ProductRepository])],
   controllers: [ProductController],
   providers: [ProductService],
 })
