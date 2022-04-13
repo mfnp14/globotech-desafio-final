5ce440ae446050a236f6febee2492f6c5e6ec31f
JonissonGomes <joni_gomes@hotmail.com>
2022-04-12 23:49:12 -0300

    Adicionando controller e services ao módulo raíz

--- src/product/product.module.ts
+++ src/product/product.module.ts
@@ -1,13 +1,15 @@
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
-import { ProductService } from './useCases/createProduct/product.service';
-import { ProductController } from './useCases/createProduct/product.controller';
 import { Product } from './entities/product.entity';
-import { ProductRepository } from './repositories/createProduct.repository';
+import { ProductRepository } from './repositories/Product.repository';
+import { ListProductController } from './useCases/listProduct/listProduct.controller';
+import { CreateProductController } from './useCases/createProduct/createProduct.controller';
+import { CreateProductService } from './useCases/createProduct/createProduct.service';
+import { ListProductService } from './useCases/listProduct/listProduct.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Product, ProductRepository])],
-  controllers: [ProductController],
-  providers: [ProductService],
+  controllers: [CreateProductController, ListProductController],
+  providers: [CreateProductService, ListProductService],
 })
 export class ProductModule {}
