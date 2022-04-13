b3d2080850ff0fb39550f51d96e128d3fa93678d
mfnp14 <marcelfnpontes@hotmail.com>
2022-04-12 21:07:59 -0300

    construir dto para products

---
+++ src/product/dto/create-product.dto.ts
@@ -0,0 +1,8 @@
+export class CreateProductDto {
+    name: string;
+    image: string;
+    description: string;
+    price: string;
+    extras: string[];
+  }
+  
---
+++ src/product/dto/return-product.dto.ts
@@ -0,0 +1,4 @@
+export class ReturnProductDto {
+    message: string;
+  }
+  
