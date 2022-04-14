7f2f970c2a7106430da291ef6e1e82223e7a5908
Claudia Maia <maiaandradec@gmail.com>
2022-04-14 05:42:19 -0300

    feat: registerRestaurant

--- frontend/jason-app/src/pages/registerRestaurant/index.tsx
+++ frontend/jason-app/src/pages/registerRestaurant/index.tsx
@@ -2,8 +2,15 @@ import { useHistory } from "react-router-dom";
 import api from "../../services/api";
 import GenericInput from "../../component/Input";
 import GenericButton from "../../component/Button";
+import React, { useState } from "react";
 
 const Register = () => {
+  const [name, setName] = useState<string>("");
+  const [logoUrl, setLogoUrl] = useState<string>("");
+  const [address, setAddress] = useState<string>("");
+  const [description, setDescription] = useState<string>("");
+  const [manager, setManager] = useState<string>("");
+
   const navigation = useHistory();
   const history = useHistory();
 
@@ -12,20 +19,18 @@ const Register = () => {
   };
 
   const storageData = async (e: any) => {
-    console.log(e)
+    console.log(e);
     try {
-      e.preventDefault();
-      const { name, address, logoUrl, description, manager } =
-        e.target.elements;
+      // e.preventDefault();
+      // const { name, address, logoUrl, description, manager } =
+      //   e.target.elements;
       const restaurantData: any = {
-        name: name.value,
-        address: address.value,
-        description: description.value,
-        logoUrl: logoUrl.value,
-        manager: manager.value,
+        name: name,
+        address: address,
+        description: description,
+        logoUrl: logoUrl,
+        manager: manager,
       };
-
-      console.log(name, address)
 
       const requestApi = await api.post("restaurant", restaurantData);
       console.log(restaurantData);
@@ -34,6 +39,7 @@ const Register = () => {
     }
   };
 
+  console.log(name, address, description, logoUrl, manager);
   return (
     <div className="container">
       <div className="logo">
@@ -43,11 +49,36 @@ const Register = () => {
         <div className="cardForm">
           <h1>Registre-se</h1>
           <form onSubmit={storageData}>
-              <GenericInput
-                type={''}
-                name={''}
-                placeholder={''}
-              />
+            <GenericInput
+              value={name}
+              type="text"
+              placeholder="Nome do restaurante"
+              onChange={(e) => setName(e.currentTarget.value)}
+            />
+            <GenericInput
+              value={address}
+              type="text"
+              placeholder="Endereço do restaurante"
+              onChange={(e) => setAddress(e.currentTarget.value)}
+            />
+            <GenericInput
+              value={description}
+              type="text"
+              placeholder="Descrição do restaurante"
+              onChange={(e) => setDescription(e.currentTarget.value)}
+            />
+            <GenericInput
+              value={manager}
+              type="text"
+              placeholder="Responsável pelo restaurante"
+              onChange={(e) => setManager(e.currentTarget.value)}
+            />
+            <GenericInput
+              value={logoUrl}
+              type="text"
+              placeholder="Nome do restaurante"
+              onChange={(e) => setLogoUrl(e.currentTarget.value)}
+            />
             <GenericButton
               secondary
               type="submit"
