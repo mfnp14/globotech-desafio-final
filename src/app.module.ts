7f7b9b5e886405559c30c7e33663ea4c4a7a1e00
JonissonGomes <joni_gomes@hotmail.com>
2022-04-13 23:48:35 -0300

    Identando módulo principal

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,7 +8,12 @@ import { RestaurantModule } from './restaurant/restaurant.module';
 import { ProductModule } from './product/product.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeOrmConfig), UsersModule, RestaurantModule, ProductModule],
+  imports: [
+    TypeOrmModule.forRoot(typeOrmConfig),
+    UsersModule,
+    RestaurantModule,
+    ProductModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
