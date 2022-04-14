36ed78c4c2284e44bab9cf00a406bcc80e70c206
JonissonGomes <joni_gomes@hotmail.com>
2022-04-14 02:22:10 -0300

    Removendo dados mocados

--- frontend/jason-app/src/pages/home/index.tsx
+++ frontend/jason-app/src/pages/home/index.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import api from "../../services/api";
-import Card, { InfoData } from "../../component/Card";
+import Card from "../../component/Card";
 import {
   ButtonPrimary,
   Container,
@@ -11,57 +11,7 @@ import {
 } from "./style";
 
 const Home = () => {
-  const restaurants: InfoData[] = [
-    {
-      name: "Doce Sabor Restaurante",
-      description: "Et do magna nulla amet cillum.",
-      address: "Rua dois, nº 0",
-      owner: "José da Silva",
-      logo: "https://d1csarkz8obe9u.cloudfront.net/posterpreviews/french-restaurant-logo-design-template-2f7896b33ab323cbc1baf240f643e82b_screen.jpg?ts=1597302426",
-    },
-    {
-      name: "Da base Restaurante",
-      description: "Et do magna nulla amet cillum.",
-      address: "Rua dois, nº 0",
-      owner: "José da Silva",
-      logo: "https://s3-sa-east-1.amazonaws.com/projetos-artes/fullsize%2F2020%2F06%2F28%2F19%2FLogo-268745_141343_195610032_635995249.jpg",
-    },
-    {
-      name: "Da base Restaurante",
-      description: "Et do magna nulla amet cillum.",
-      address: "Rua dois, nº 0",
-      owner: "José da Silva",
-      logo: "https://s3-sa-east-1.amazonaws.com/projetos-artes/fullsize%2F2020%2F06%2F28%2F19%2FLogo-268745_141343_195610032_635995249.jpg",
-    },
-    {
-      name: "Da base Restaurante",
-      description: "Et do magna nulla amet cillum.",
-      address: "Rua dois, nº 0",
-      owner: "José da Silva",
-      logo: "https://s3-sa-east-1.amazonaws.com/projetos-artes/fullsize%2F2020%2F06%2F28%2F19%2FLogo-268745_141343_195610032_635995249.jpg",
-    },
-    {
-      name: "Da base Restaurante",
-      description: "Et do magna nulla amet cillum.",
-      address: "Rua dois, nº 0",
-      owner: "José da Silva",
-      logo: "https://s3-sa-east-1.amazonaws.com/projetos-artes/fullsize%2F2020%2F06%2F28%2F19%2FLogo-268745_141343_195610032_635995249.jpg",
-    },
-    {
-      name: "Da base Restaurante",
-      description: "Et do magna nulla amet cillum.",
-      address: "Rua dois, nº 0",
-      owner: "José da Silva",
-      logo: "https://s3-sa-east-1.amazonaws.com/projetos-artes/fullsize%2F2020%2F06%2F28%2F19%2FLogo-268745_141343_195610032_635995249.jpg",
-    },
-    {
-      name: "Da base Restaurante",
-      description: "Et do magna nulla amet cillum.",
-      address: "Rua dois, nº 0",
-      owner: "José da Silva",
-      logo: "https://s3-sa-east-1.amazonaws.com/projetos-artes/fullsize%2F2020%2F06%2F28%2F19%2FLogo-268745_141343_195610032_635995249.jpg",
-    },
-  ];
+
 
   const [restauranteData, setRestauranteData] = useState([]);
 
