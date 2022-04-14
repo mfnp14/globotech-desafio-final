4285a8e495207f952dd8bd4e7aa8b25214c0ee0a
JonissonGomes <joni_gomes@hotmail.com>
2022-04-14 01:41:27 -0300

    Inserindo Cors Header

--- frontend/jason-app/src/services/api.tsx
+++ frontend/jason-app/src/services/api.tsx
@@ -2,6 +2,14 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: 'http://localhost:4000/',
+  headers: {
+    "Access-Control-Allow-Origin":
+      "http://localhost:3333, http://localhost:4000",
+    "Access-Control-Allow-Headers": "Authorization",
+    "Access-Control-Allow-Methods":
+      "GET, POST, OPTIONS, PUT, PATCH, DELETE",
+    "Content-Type": "application/json;charset=UTF-8",
+  },
 });
 
 export default api;
