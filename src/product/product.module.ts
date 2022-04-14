52a31b3ea55609d755bf148774e4feac566e2cc8
JonissonGomes <joni_gomes@hotmail.com>
2022-04-13 23:48:24 -0300

    Adicionando Controllers, Services e Módulos ao módulo principal [Extra]

--- src/product/product.module.ts
+++ src/product/product.module.ts
@@ -6,10 +6,25 @@ import { ListProductController } from './useCases/listProduct/listProduct.contro
 import { CreateProductController } from './useCases/createProduct/createProduct.controller';
 import { CreateProductService } from './useCases/createProduct/createProduct.service';
 import { ListProductService } from './useCases/listProduct/listProduct.service';
+import { Extra } from './entities/extra.entity';
+import { ExtraRepository } from './repositories/Extra.repository';
+import { CreateExtraController } from './useCases/createExtra/createExtra.controller';
+import { CreateExtraService } from './useCases/createExtra/createExtra.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Product, ProductRepository])],
-  controllers: [CreateProductController, ListProductController],
-  providers: [CreateProductService, ListProductService],
+  imports: [
+    TypeOrmModule.forFeature([
+      Product,
+      Extra,
+      ProductRepository,
+      ExtraRepository,
+    ]),
+  ],
+  controllers: [
+    CreateProductController,
+    CreateExtraController,
+    ListProductController,
+  ],
+  providers: [CreateProductService, CreateExtraService, ListProductService],
 })
 export class ProductModule {}
